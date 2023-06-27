0ba82c5cc575493284d1c8485560f0103f431dbb
Yuri Corrêa <yuri.correa1@outlook.com>
2023-06-27 19:56:47 -0300

    feat: install lib expo-fonts and configure theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts
+} from '@expo-google-fonts/nunito-sans'
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Daily diet</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
