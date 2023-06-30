6de90013807efb6bc6e36b07c10d5162f81445c3
Yuri Corrêa <yuri.correa1@outlook.com>
2023-06-30 00:06:48 -0300

    feat: input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import { TextInputProps, StyleProp, TextInput } from "react-native";
+import { Wrapper, Container, Label } from "./styles";
+
+type Props = TextInputProps & {
+  label: string;
+}
+
+
+export function Input({ label, ...rest }: Props) {
+  return (
+    <Wrapper >
+      <Label>{label}</Label>
+
+      <Container  {...rest} />
+    </Wrapper>
+  );
+}
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,30 @@
+import { TextInput } from 'react-native';
+import styled from 'styled-components/native';
+
+
+export const Wrapper = styled.View`
+  width: 100%;
+  margin-bottom: 24px;
+
+`;
+
+export const Container = styled(TextInput)`
+  width: 100%;
+  height:  48px;
+
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_500};
+  border-radius: 6px;
+  
+  padding: 16px;
+
+`;
+
+export const Label = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.XS}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+  margin-bottom: 4px;
+`;
+
